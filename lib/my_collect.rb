f50fcac4b9163941ae4b2a9caca3f197fc0bbcc0
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-11 02:13:35 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,8 @@ def my_collect(collection)
   i = 0
 
   empty_array = []
-  while i < collection.length
-    i = i + 1
-
+    while i < collection.length
+      empty_array << yield(collection[i])
+      i = i + 1
+    end
 end
